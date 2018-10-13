173b2199a7b5677ce68c2cbc873ac60ce8fb780b
devsave <tiantangtl@foxmail.com>
2018-10-13 21:21:30 +0800

    Change the structure.

--- src/logutils/__init__.py
+++ src/logutils/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# @Time    : 2018/10/13 18:50
+# @Time    : 2018/10/13 21:16
 # @Author  : Tony Tian
 # @Email   : tiantangtl@foxmail.com
-# @File    : __init__.py
+# @File    : __init__.py.py
 
-__all__ = ['regex_checker']
+
